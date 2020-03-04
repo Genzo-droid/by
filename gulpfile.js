799ef4026e5a466bb6d003324da3e7414cfc2ffb
genzo-droid <genzo.by@gmail.com>
2020-03-04 23:07:04 +0300

    choe: prepared workspace

--- gulpfile.js
+++ gulpfile.js
@@ -11,16 +11,16 @@ function bs() {
       }
   });
   watch("./*.html").on('change', browserSync.reload);
-  watch("./sass/**/.sass", serveSass);
+  watch("./sass/**/*.sass", serveSass);
   watch("./js/*.js").on('change', browserSync.reload);
 };
 
 // Compile sass into CSS & auto-inject into browsers
 function serveSass() {
   return src("./sass/*.sass")
-      .pipe(sass())
-      .pipe(dest("./css"))
-      .pipe(browserSync.stream());
+    .pipe(sass())
+    .pipe(dest("./css"))
+    .pipe(browserSync.stream());
 };
 
 exports.serve = bs;
